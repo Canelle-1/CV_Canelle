47b805f86f7a98e35200dfab856dcdd550b650cf
Canelle-1 <rault.e2200599@etud.univ-ubs.fr>
2025-06-04 15:19:25 +0200

    Update ScriptCV.js

--- ScriptCV.js
+++ ScriptCV.js
@@ -1,20 +1,18 @@
-
 function photo(voyage) {
     var image;
-    if (voyage=="Prague") {
-        image = "photo_pragues.jpeg";
-    } else if (voyage=="Toulouse") {
-        image = "photo_toulouse.jpeg";
-    } else if (voyage=="Olbia") {
-        image = "photo_olbia.jpeg";
-    } else if (voyage=="Athènes") {
-        image = "photo_athènes.jpeg";
-   
-    } else if (voyage=="Naples") {
-        image = "photo_naples.jpeg";
-    } else if (voyage=="Porto") {
-        image = "photo_porto.jpeg";
+    if (voyage == "Prague") {
+        image = "/CV_Canelle/photo_pragues.jpeg";
+    } else if (voyage == "Toulouse") {
+        image = "/CV_Canelle/photo_toulouse.jpeg";
+    } else if (voyage == "Olbia") {
+        image = "/CV_Canelle/photo_olbia.jpeg";
+    } else if (voyage == "Athènes") {
+        image = "/CV_Canelle/photo_athènes.jpeg";
+    } else if (voyage == "Naples") {
+        image = "/CV_Canelle/photo_naples.jpeg";
+    } else if (voyage == "Porto") {
+        image = "/CV_Canelle/photo_porto.jpeg";
     }
+
     document.getElementById("monImage").src = image;
 }
-
